a1d6561cc1c9544706a072f2a767c2f3a30356f1
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-29 10:07:22 +0300

    Work in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+
+  N = (pairs[pairs.length - 1][0] ** pairs[pairs.length - 1][1])
+
+  How many such integer numbers k exist (0 <= k <= N), which follow next condition: 
+  greatest common divisor of (k + j and N) is 1 if s[j] === 1 AND 
+  greatest common divisor of (k + j and N) is NOT 1 if s[j] === 0.
+
+  // answer - task solution
+  const result = answer % 1000000007;
+  return result;
 }
